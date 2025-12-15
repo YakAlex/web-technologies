03ac218300e1dcff738329e7ce99071ca8f52dc6
yakalex <yakimenkoalex03@gmail.com>
2025-12-15 10:09:00 +0200

    Fix changes

--- lab tasks/lab6/script.js
+++ lab tasks/lab6/script.js
@@ -1,20 +1,15 @@
-// Знаходимо кнопку і покемона
 const btn = document.getElementById('toggle-btn');
 const pokemon = document.querySelector('.pokemon');
 const clouds = document.querySelectorAll('.cloud');
 const sky = document.querySelector('.sky');
 
-let isRunning = true; // Стан анімації
+let isRunning = true;
 
 btn.addEventListener('click', () => {
     if (isRunning) {
-        // ЯКЩО ПРАЦЮЄ -> ЗУПИНИТИ
-
-        // Зупиняємо анімацію покемона (ставимо на паузу)
         pokemon.style.animationPlayState = 'paused';
         sky.style.animationPlayState = 'paused';
 
-        // Зупиняємо кожну хмару
         clouds.forEach(cloud => {
             cloud.style.animationPlayState = 'paused';
         });
@@ -22,7 +17,6 @@ btn.addEventListener('click', () => {
         btn.textContent = 'Start';
         isRunning = false;
     } else {
-        // ЯКЩО СТОЇТЬ -> ЗАПУСТИТИ
 
         pokemon.style.animationPlayState = 'running';
         sky.style.animationPlayState = 'running';
